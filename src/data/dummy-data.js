77e418f0d4e2dc44961a2517ce2b64e8a128eae7
Jaiane Oliveira <jaianeoliveira.dev@gmail.com>
2022-05-22 22:52:44 -0300

    feat: create a dummy data

---
+++ src/data/dummy-data.js
@@ -0,0 +1,76 @@
+// read
+// readings
+// want to read
+// abandoned
+
+// position
+// percent
+// page
+
+export default [
+	{
+		user_id: 'd57e8b84-e8b9-4bba-b8be-ff3c85c38b30',
+		authentication_data: {
+			email: 'jaiane@email.com',
+			password: 'secret',
+		},
+		user_data: {
+			first_name: 'Jaiane',
+			last_name: 'Oliveira',
+			email: 'jaiane@email.com',
+			profile_photo:
+				'https://scontent-for1-1.cdninstagram.com/v/t51.2885-15/283809545_565592721824787_8196599832537273130_n.webp?stp=dst-jpg_e35&_nc_ht=scontent-for1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=Aw8A60H_B40AX_Vok3k&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjg0NDAwNzQyMzU2NDYwMjI3Nw%3D%3D.2-ccb7-5&oh=00_AT9nerbRpTQ6fMkrWvnzcFTj_Nv5ftXX-l_oTuwvP5rFhA&oe=62911B78&_nc_sid=30a2ef',
+		},
+		books: [
+			{
+				id: 'dc910f1e-99d7-454d-ad04-558a8a14c7af',
+				title: 'A biblioteca da Meia Noite',
+				author: 'Matt Haig',
+				capa: 'https://images-na.ssl-images-amazon.com/images/I/51kAYMwbQIL._SY344_BO1,204,203,200_QL70_ML2_.jpg',
+				status: 'READ',
+				bookmarks: [
+					{
+						id: '9f8ac639-c02a-46fe-aeec-37de78035fc0',
+						excerpt: null,
+						position: {
+							type: 'POSITION',
+							position: 1234,
+						},
+						comment: null,
+					},
+				],
+			},
+			{
+				id: '80ce2246-ab10-4490-a898-b07891b85b93',
+				title: 'A viajante do tempo',
+				author: 'Diana Gabaldon',
+				capa: 'https://images-na.ssl-images-amazon.com/images/I/51Xyr1ziTEL._SX346_BO1,204,203,200_.jpg',
+				status: 'READING',
+				bookmarks: [],
+			},
+			{
+				id: 'aeca7e70-3d43-4820-aa83-466b1aba680c',
+				title: 'Torre do Alvorecer',
+				author: 'Sarah J. Maas',
+				capa: 'https://images-na.ssl-images-amazon.com/images/I/51fMabcuUwL._SY344_BO1,204,203,200_QL70_ML2_.jpg',
+				status: 'READING',
+				bookmarks: [],
+			},
+		],
+	},
+	{
+		user_id: 'b813bad3-0503-40d7-a3d6-74d970b8b33a',
+		authentication_data: {
+			email: 'jiminshi@email.com',
+			password: 'anyong',
+		},
+		user_data: {
+			first_name: 'Jimin',
+			last_name: 'Park',
+			email: 'jiminshi@email.com',
+			profile_photo:
+				'https://scontent-for1-1.cdninstagram.com/v/t51.2885-15/275722620_497692755339612_8572135863206933327_n.webp?stp=dst-jpg_e35&_nc_ht=scontent-for1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=l1ApOWBNpAgAX85dK7Z&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc5Mzc4OTI2MzU1NjU1NTkzMw%3D%3D.2-ccb7-5&oh=00_AT-9zZ7-R5_IMm-Kj7VHMt9APamaIlpSoblGnPRcuY3Clw&oe=6291C898&_nc_sid=30a2ef',
+		},
+		books: [],
+	},
+];
